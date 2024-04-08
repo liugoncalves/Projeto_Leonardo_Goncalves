0a44c3183be46b115972a3d43f6819316a9bbe7f
Leo Gonçalves <leonardo.flora@estudante.ufla.br>
2024-04-07 23:37:29 -0300

    Criada a classe D com metodos MD1() e MD2()

---
+++ D.java
@@ -0,0 +1,34 @@
+public class D {
+
+    private String D1;
+    private int D2;
+
+    public D(String D1, int D2) {
+        this.D1 = D1;
+        this.D2 = D2;
+    }
+
+    public String getD1() {
+        return D1;
+    }
+
+    public void setD1(String D1) {
+        this.D1 = D1;
+    }
+
+    public int getD2() {
+        return D2;
+    }
+
+    public void setD2(int D2) {
+        this.D2 = D2;
+    }
+
+    public void MD1() {
+        System.out.println("Método MD1 da classe D");
+    }
+
+    public void MD2() {
+        System.out.println("Método MD2 da classe D");
+    }
+}
